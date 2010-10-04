62488b636a9608797cff1b6170a2e0e95a724fc4
rgaudin <rgaudin@gmail.com>
2010-10-04 18:20:50 +0000

    added parent field to admin list

--- admin.py
+++ admin.py
@@ -15,7 +15,7 @@ class AreaTypeAdmin(admin.ModelAdmin):
 
 
 class AreaAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'name', 'kind', 'location', 'code')
+    list_display = ('__unicode__', 'name', 'kind', 'location', 'code', 'parent')
     search_fields = ['code', 'name']
     list_filter = ('kind',)
 
